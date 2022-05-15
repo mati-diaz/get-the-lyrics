84caf11efe93ad3ff62c3d75f06ff9e95e064314
Matias Diaz <matidiazruiz42@gmail.com>
2022-05-15 20:39:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Loading } from "./Loading";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  // Form
+  const [formValues, setFormValues] = useState({
+    artist: 'coldplay',
+    song: 'Adventure of a Lifetime'
+  });
+  const { artist, song } = formValues;
+
+  const handleInputChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!artist) return toast.error('Se necesita un artista');
+    if (!song) return toast.error('Se necesita una canción');
+
+    getLyrics(`https://api.lyrics.ovh/v1/${artist}/${song}`);
+  }
+
+  // Data
+  const [data, setData] = useState({
+    lyrics: '',
+    loading: false
+  });
+  const { lyrics, loading } = data;
+
+  const getLyrics = async (url) => {
+    setData({
+      ...data,
+      loading: true
+    });
+    const res = await fetch(url);
+    const { lyrics } = await res.json();
+    setData({
+      ...data,
+      lyrics,
+      loading: false
+    });
+  }
+
+  if (loading) return <Loading />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <ToastContainer />
+      <h1 className="app__title">Get The Lyrics</h1>
+      <form onSubmit={ handleSubmit } className='form'>
+        <input
+          type="text"
+          placeholder="Introduce el nombre del cantante/grupo"
+          name="artist"
+          value={ artist }
+          onChange={ handleInputChange }
+          className='form__input'
+        />
+        <input
+          type="text"
+          placeholder="Introduce el nombre de la canción"
+          name="song"
+          value={ song }
+          onChange={ handleInputChange }
+          className='form__input'
+        />
+        <button
+          type="submit"
+          className="form__button"
         >
-          Learn React
-        </a>
-      </header>
+          Buscar
+        </button>
+      </form>
+      {
+        lyrics &&
+        <p className="app__lyrics">{ lyrics }</p>
+      }
     </div>
   );
 }
