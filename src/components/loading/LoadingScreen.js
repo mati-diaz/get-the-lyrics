b8ef19b87c1e442b7c3619945892c29ee81d5e6f
Matias Diaz <matidiazruiz42@gmail.com>
2022-05-16 19:41:04 -0300

    loading screen

--- src/components/loading/LoadingScreen.js
+++ src/components/loading/LoadingScreen.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Loading } from './styles'
+import { GlobalStyle } from '../../styles'
+import { Loader, Loading } from './styles'
 
 export const LoadingScreen = () => {
   return (
     <Loading>
+      <GlobalStyle />
+      <Loader />
       <p>Buscando</p>
     </Loading>
   )
--- src/components/loading/styles.js
+++ src/components/loading/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 
 export const Loading = styled.div`
   position: absolute;
@@ -7,6 +8,27 @@ export const Loading = styled.div`
   font-size: 24px;
   font-weight: 500;
   display: flex;
+  flex-direction: column;
+  gap: 10px;
   justify-content: center;
   align-items: center;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loader = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  border: 5px solid white;
+  border-top: 5px solid rgb(0, 145, 255);
+  animation: ${rotate} 2s linear infinite;
 `;
